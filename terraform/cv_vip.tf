40b12cb6826024a71959f3be61471392c2499d17
Finn-Thorben Sell <dev@finn-thorben.me>
2023-08-28 01:18:00 +0200

    remove cv dns record

--- terraform/cv_vip.tf
+++ terraform/cv_vip.tf
@@ -5,13 +5,6 @@ resource "hcloud_floating_ip" "cv_vip" {
   server_id   = hcloud_server.main.id
 }
 
-resource "hetznerdns_record" "cv_vip" {
-  zone_id = data.hetznerdns_zone.ftsell_de.id
-  type    = "AAAA"
-  name    = "cv"
-  value   = "${hcloud_floating_ip.cv_vip.ip_address}42"
-}
-
 output "cv_vip_base" {
   value = hcloud_floating_ip.cv_vip.ip_address
 }
