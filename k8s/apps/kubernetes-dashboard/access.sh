e33c5e14363d6334e4a251d16d36ce7a64adf007
ftsell <dev@ftsell.de>
2024-07-17 16:03:08 +0200

    move kubernetes-dashboard into apps folder

--- k8s/apps/kubernetes-dashboard/access.sh
+++ k8s/apps/kubernetes-dashboard/access.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-TOKEN=$(kubectl -n kubernetes-dashboard create token dashboard-user)
+TOKEN=$(kubectl --context=ftsell-de -n kubernetes-dashboard create token dashboard-user)
 
 echo "Url:        :  http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
 echo ""
 echo "Access Token:  $TOKEN"
 
-kubectl proxy > /dev/null
+kubectl --context=ftsell-de proxy > /dev/null
