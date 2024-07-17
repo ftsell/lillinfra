356bbd1e66312d4b10f32e1356b409139e0cf222
ftsell <dev@ftsell.de>
2024-07-17 16:06:57 +0200

    improve dashboards access.sh script

--- k8s/apps/kubernetes-dashboard/access.sh
+++ k8s/apps/kubernetes-dashboard/access.sh
@@ -2,9 +2,10 @@
 set -eu
 
 TOKEN=$(kubectl --context=ftsell-de -n kubernetes-dashboard create token dashboard-user)
+wl-copy "$TOKEN"
 
 echo "Url:        :  http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
 echo ""
-echo "Access Token:  $TOKEN"
+echo "Access Token (copied to clipboard):  $TOKEN"
 
 kubectl --context=ftsell-de proxy > /dev/null
