50ac75060688cf2549c9e96b80c345f076081719
ftsell <dev@ftsell.de>
2024-09-03 21:29:51 +0200

    sort system definitions

--- systems/default.nix
+++ systems/default.nix
@@ -25,13 +25,22 @@ let
   };
 in
 {
+  # exposed hosts at myroot
   "hosting.srv.ftsell.de" = mkSystem "x86_64-unknown-linux-gnu" "hosting.srv.ftsell.de" inputs.nixpkgs-small;
   "rt-hosting.srv.ftsell.de" = mkSystem "x86_64-unknown-linux-gnu" "rt-hosting.srv.ftsell.de" inputs.nixpkgs-small;
-  "main.srv.ftsell.de" = mkSystem "x86_64-unknown-linux-gnu" "main.srv.ftsell.de" inputs.nixpkgs-small;
   "mail.srv.ftsell.de" = mkSystem "x86_64-unknown-linux-gnu" "mail.srv.ftsell.de" inputs.nixpkgs-small;
   "gtw.srv.ftsell.de" = mkSystem "x86_64-unknown-linux-gnu" "gtw.srv.ftsell.de" inputs.nixpkgs-small;
-  vpn-srv = mkSystem "x86_64-unknown-linux-gnu" "vpn-srv" inputs.nixpkgs-small;
+
+  # internal hosts at myroot
+  "main.srv.myroot.intern" = mkSystem "x86_64-unknown-linux-gnu" "main.srv.mroot.intern" inputs.nixpkgs-small;
+  "vpn.srv.myroot.intern" = mkSystem "x86_64-unknown-linux-gnu" "vpn.srv.myroot.intern" inputs.nixpkgs-small;
+
+  # private systems
   finnsLaptop = mkSystem "x86_64-unknown-linux-gnu" "finnsLaptop" inputs.nixpkgs;
+
+  # home systems
   "finnsWorkstation.home.private" = mkSystem "x86_64-unknown-linux-gnu" "finnsWorkstation" inputs.nixpkgs;
+
+  # others
   "factorio.z9.ccchh.net" = mkSystem "x86_64-unknown-linux-gnu" "factorio.z9.ccchh.net" inputs.nixpkgs;
 }
