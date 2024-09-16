cc67a2abbe6e86f99e4eff5f5bb70115787f0bc3
ftsell <dev@ftsell.de>
2024-09-16 17:59:33 +0200

    fix bgp routing on hosting

--- systems/rt-hosting.srv.ftsell.de.nix
+++ systems/rt-hosting.srv.ftsell.de.nix
@@ -148,23 +148,20 @@ in
         router bgp 214493
           no bgp default ipv4-unicast
           bgp default ipv6-unicast
+          bgp ebgp-requires-policy
           
           neighbor myroot peer-group
           neighbor myroot remote-as 39409
           neighbor myroot capability dynamic
           neighbor 2a10:9906:1002::2 peer-group myroot
 
-          # neighbor myroot prefix-list pl-allowed-import in
-          # neighbor myroot prefix-list pl-allowed-export out
-
           address-family ipv6 unicast
-             network 2a10:9902:111::/48
-             # neighbor myroot prefix-list pl-allowed-export out
-             neighbor myroot prefix-list pl-allowed-import in
+            redistribute static
+            neighbor myroot prefix-list pl-allowed-export out
+            neighbor myroot prefix-list pl-allowed-import in
           exit-address-family
         
         ip prefix-list pl-allowed-import seq 5 permit ::/0
-
         ip prefix-list pl-allowed-export seq 5 permit 2a10:9902:111::/48
       '';
     };
