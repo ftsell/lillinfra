4903d3cdf5bd0474acf16efa2b6128fd48da4217
lilly <li@lly.sh>
2024-11-08 16:03:00 +0100

    update factorio.z9 config

--- nix/systems/factorio.z9.ccchh.net.nix
+++ nix/systems/factorio.z9.ccchh.net.nix
@@ -4,71 +4,27 @@ let
 in
 {
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
+    ../modules/hosting_guest.nix
     ../modules/base_system.nix
     ../modules/user_ftsell.nix
   ];
 
-  disko.devices = {
-    disk.disk1 = {
-      device = "/dev/vda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          mbr = {
-            size = "1M";
-            type = "EF02";
-            priority = 1;
-          };
-          boot = {
-            name = "boot";
-            size = "500M";
-            type = "8300";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          };
-          swap = {
-            size = "2G";
-            type = "8200";
-            content = {
-              type = "swap";
-            };
-          };
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "bcachefs";
-              mountpoint = "/";
-            };
-          };
-        };
-      };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/5580383e-c67b-4b7c-8776-9b5867fb8daf";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/7159-8B6F";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
     };
   };
+  swapDevices = [{ 
+    device = "/dev/disk/by-uuid/3a8d7425-6cef-4025-92a0-bad0b73dcf4e";
+  }];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "ahci" "virtio_pci" "sr_mod" "virtio_blk" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-  boot.loader.grub.enable = true;
   networking.useDHCP = false;
-
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
-    };
-  };
-
-  services.qemuGuest.enable = true;
-
   systemd.network = {
     enable = true;
     networks.ethMyRoot = {
@@ -76,24 +32,22 @@ in
         Type = "ether";
         MACAddress = "bc:24:11:a3:43:7f";
       };
-      networkConfig = {
-        DHCP = "yes";
-      };
+      networkConfig."DHCP" = "yes";
     };
   };
 
+  # actual factorio config
+  #services.factorio = {
+  #  enable = true;
+  #  game-name = "CCCHH Factorio";
+  #  lan = true;
+  #  saveName = "default-no-mods";
+  #  requireUserVerification = false;
+  #  openFirewall = true;
+  #};
+  
   # DO NOT CHANGE
   # this defines the first version of NixOS that was installed on the machine so that programs with non-migratable data files are kept compatible
   system.stateVersion = "24.05";
   home-manager.users.ftsell.home.stateVersion = "24.05";
-
-  # actual factorio config
-  services.factorio = {
-    enable = true;
-    game-name = "CCCHH Factorio";
-    lan = true;
-    saveName = "default-no-mods";
-    requireUserVerification = false;
-    openFirewall = true;
-  };
 }
